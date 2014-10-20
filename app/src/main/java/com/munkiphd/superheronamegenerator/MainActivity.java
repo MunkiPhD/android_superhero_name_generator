6b41b121ddea6d6bc7ae41be3d866c2c8badd61f
MunkiPhD <munkiphd@gmail.com>
2014-10-20 16:37:37 -0400

    initial commit

---
+++ app/src/main/java/com/munkiphd/superheronamegenerator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.munkiphd.superheronamegenerator;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void GenerateSuperHeroName(View view){
+        EditText nameEntry = (EditText)findViewById(R.id.person_name);
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(nameEntry.getWindowToken(), 0);
+
+        String name = nameEntry.getText().toString();
+        TextView heroName = (TextView)findViewById(R.id.textSuperHeroName);
+        heroName.setText(name);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
